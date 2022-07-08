1d41da4d5aad5ed05a1ff291e90f4abd27467598
returner <returner@daum.net>
2022-07-08 12:12:30 +0900

    async vs multithreading

---
+++ DotNetBestPractice/AsyncAwait/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var firstAsync = FirstAsync();
+var secondAsync = SecondAsync();
+var thirdAsync = ThirdAsync();
+
+await Task.WhenAll(firstAsync, secondAsync, thirdAsync);
+
+
+async Task FirstAsync()
+{
+    Console.WriteLine("First Async Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+    await Task.Delay(1000);
+    Console.WriteLine("First Async Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+}
+async Task SecondAsync()
+{
+    Console.WriteLine("Second Async Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+    await Task.Delay(1000);
+    Console.WriteLine("Second Async Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+}
+async Task ThirdAsync()
+{
+    Console.WriteLine("Third Async Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+    await Task.Delay(1000);
+    Console.WriteLine("Third Async Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+}
---
+++ DotNetBestPractice/Multithreading/Multithreading.cs
@@ -0,0 +1,33 @@
+﻿public class Multithreading
+{
+    public void FirstMethod()
+    {
+        Console.WriteLine("First Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        Thread.Sleep(1000);
+        Console.WriteLine("First Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+    }
+    public void SecondMethod()
+    {
+        Console.WriteLine("Second Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        Thread.Sleep(1000);
+        Console.WriteLine("Second Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+    }
+    public void ThirdMethod()
+    {
+        Console.WriteLine("Third Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        Thread.Sleep(1000);
+        Console.WriteLine("Third Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+    }
+
+    public void ExecuteMultithreading()
+    {
+        var t1 = new Thread(new ThreadStart(FirstMethod));
+        var t2 = new Thread(new ThreadStart(SecondMethod));
+        var t3 = new Thread(new ThreadStart(ThirdMethod));
+
+        t1.Start();
+        t2.Start();
+        t3.Start();
+    }
+}
+
