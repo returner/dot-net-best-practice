01942bdcc98229509ac83bf93484c01d2cb594f2
returner <returner@daum.net>
2022-07-08 12:04:46 +0900

    add using

---
+++ DotNetBestPractice/Using/Inner.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace Using
+{
+    using System;
+    using System.Text;
+
+    public class Inner
+    {
+        public string Test()
+        {
+            var sb = new StringBuilder(20);
+            sb.Append(Guid.NewGuid().ToString());
+            return sb.ToString();
+        }
+    }
+
+    public class Inner2
+    {
+        public string Test()
+        {
+            var sb = new StringBuilder(20);
+            sb.Append(Guid.NewGuid().ToString());
+            return sb.ToString();
+        }
+    }
+}
+
+public class Outer
+{
+    public string Test()
+    {
+        var sb = new StringBuilder(20);
+        sb.Append(Guid.NewGuid().ToString());
+        return sb.ToString();
+    }
+}
