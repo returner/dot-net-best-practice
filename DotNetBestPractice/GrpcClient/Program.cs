d0dfa10f90f8e553cda8cf92930f186926a66438
returner <returner@daum.net>
2022-07-08 17:48:44 +0900

    gRpc example

---
+++ DotNetBestPractice/GrpcClient/Program.cs
@@ -0,0 +1,7 @@
+﻿using Grpc.Net.Client;
+using GrpcGreet;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7078");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine($"Greeting : {reply.Message}");
