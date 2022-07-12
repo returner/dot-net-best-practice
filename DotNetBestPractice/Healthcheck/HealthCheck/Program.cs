0a8d8592dc4339d6a7b0900407f01b65cdd7685d
returner <returner@daum.net>
2022-07-12 11:12:05 +0900

    healthcheck

---
+++ DotNetBestPractice/Healthcheck/HealthCheck/Program.cs
@@ -0,0 +1,8 @@
+using Microsoft.Graph;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
---
+++ DotNetBestPractice/Healthcheck/Web/Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("health")]
+        public IActionResult GetHealthStatus()
+        {
+            return Ok();
+        }
+    }
+}
